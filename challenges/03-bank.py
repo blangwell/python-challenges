4959f08bef833e08f3b7c72a59bff7afea01ea56
blangwell <b.langwell@outlook.com>
2020-09-14 11:57:12 -0700

    [bank] Add view, withdraw, and deposit functions

--- challenges/03-bank.py
+++ challenges/03-bank.py
@@ -1,3 +1,40 @@
-print("Welcome to Chase bank.")
-print("Have a nice day!")
+# print("Welcome to Chase bank.")
+# print("Have a nice day!")
 
+balance = 10000
+def view_balance(balance):
+  print('Your current balance is {}'.format(balance)) 
+
+def withdraw(balance):
+  amount = input('How much money would you like to withdraw?\n')
+  amount = int(amount)
+  if amount > balance:
+    print('Insufficient funds')
+  elif amount == balance:
+    print('Thats all yr dang money though!')
+  else:
+    print('Successfully withdrew {} dollars'.format(amount))
+    print('Your remaining balance is {}'.format(balance - amount))
+    return balance - amount
+
+def deposit(balance):
+  amount = input('How much money would you like to deposit?\n')
+  amount = int(amount)
+  print('Successfully deposited {} dollars '.format(amount))
+  print('Your current balance is {} dollars '.format(balance + amount))
+  return balance + amount
+
+
+def banking(balance):
+  user_input = input('View Balance, Withdraw, or Deposit?\n')
+  if user_input.lower() == 'v':
+    view_balance(balance)
+  elif user_input.lower() == 'w':
+    print('Lets withdraw some dang money\n') 
+    balance = withdraw(balance)
+  elif user_input.lower() == 'd':
+    balance = deposit(balance)
+  else:
+    print('command not found, please press w, v, or d')
+
+banking(balance)
